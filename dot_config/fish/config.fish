130887dca3de3ed8f8e5dfc92e9a9861a24cfbcd
Asen Lekov <asenlekoff@gmail.com>
2024-04-26 02:31:32 +0300

    remove atuin

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -50,10 +50,9 @@ end
 set -U fish_user_paths /usr/local/bin $fish_user_paths
 
 
-# Configure mise and atuin
+# Configure mise
 if status is-interactive
   mise activate fish | source
-  atuin init fish | source
 else
   mise activate fish --shims | source
 end
